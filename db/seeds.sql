4c515f640f94c0035c3cd3fb06da46a26ccb8579
Adam Wheeler <farfromruin@aol.com>
2020-02-03 18:03:44 -0800

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+	values('Double Cheese Burger', false),
+    ('Bacon Swiss Burger', false),
+    ('Hawaiian Burger', false);
+    
+    select * from burgers;
